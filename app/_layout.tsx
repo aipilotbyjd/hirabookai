6676b359ce0da16a781ce06c10b23e8547a328c3
Jaydeep Dhrangiya <recentpagetoday@gmail.com>
2024-12-01 10:11:42 +0530

    p;lk

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import '../global.css';
 
-import "@/global.css";
+// import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 
 import { Stack } from 'expo-router';
